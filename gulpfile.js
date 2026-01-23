9ab63286f3aedfa6de4fdcd1614626639ff81d01
z4qu <59421804+z4qu@users.noreply.github.com>
2026-01-23 01:12:47 +0100

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
-var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
+const { src, dest, series } = require('gulp');
+const cleanCSS = require('gulp-clean-css');
 
-gulp.task('buildcss', function(){
-	gulp.src('assets-dev/styles/*.css')
-	.pipe(cleanCSS())
-	.pipe(gulp.dest('assets-prod/styles'));
-});
+function buildcss() {
+  return src('assets-dev/styles/*.css')
+    .pipe(cleanCSS())
+    .pipe(dest('assets-prod/styles'));
+}
 
-gulp.task('copyimages', function(){
-	gulp.src('assets-dev/images/*.png')
-	.pipe(gulp.dest('assets-prod/images'));
-});
+function copyimages() {
+  return src('assets-dev/images/*.png')
+    .pipe(dest('assets-prod/images'));
+}
+
+exports.buildcss = buildcss;
+exports.copyimages = copyimages;
+exports.buildapp = series(buildcss, copyimages);
